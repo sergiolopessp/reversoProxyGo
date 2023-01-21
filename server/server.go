9095cbc372b7fda7b61db12bf8e0a53741f73ad0
Sergio Lopes <sergiolopessp@gmail.com>
2023-01-21 15:02:31 -0300

    refatorando o codigo

--- server/server.go
+++ server/server.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	servidprOrigemHandler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+	servidorOrigemHandler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		fmt.Printf("[Servidor de Origem] recebida requisicao em: %s\n", time.Now())
 		_, _ = fmt.Fprintf(rw, "resposta do servidor de origem")
 	})
 
-	log.Fatal(http.ListenAndServe(":8081", servidprOrigemHandler))
+	log.Fatal(http.ListenAndServe(":8081", servidorOrigemHandler))
 }
